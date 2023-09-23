30cf00d4b5f3626e7900d7384b0427df707fc309
Sainath <sainath@analyticsquad4.com>
2023-09-23 14:13:50 +0530

    First Commit

---
+++ app/components/Authentication/LoginComponent.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import Link from 'next/link'
+import React from 'react'
+import Divider from './Divider';
+import { CLIENT_ID } from '@/constants';
+const LoginComponent = () => {
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+
+      <div className='flex flex-col justify-between items-center w-4/12 h-5/6'>
+        <div className='flex flex-col items-center justify-between flex-1 pt-20 pb-10'>
+          <div className='text-3xl color-black'>Sign in to NameSpace</div>
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+            shape='pill'
+            // theme='filled_blue'
+            context='signin'
+            text='continue_with'
+            width="20vw"
+          />
+          <Divider/>
+        </div>
+        <div className='flex flex-col items-center justify-between flex-1 w-full h-1/2'>
+        <input placeholder='Email' className='border-solid border-2 focus:border-slate-600 border-gray-200 bg-[#f4f2f2] rounded-full px-3 py-2 w-8/12 color-black placeholder:text-[#313030]'></input>
+        <input placeholder='Password' type='password' className='border-solid border-2 focus:border-slate-600 border-gray-200 bg-[#f4f2f2] rounded-full px-3 py-2 w-8/12 color-black placeholder:text-[#313030]'></input>
+
+          <button className='bg-[#f6f44d] rounded-full px-6 py-2'>Log in</button>
+          <div className='text-[#b4a9a9]'>No account?<Link href={"/signup"} className='text-[#3c64be] pl-1'>Create One</Link></div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default LoginComponent
---
+++ constants.tsx
@@ -0,0 +1,1 @@
+export const CLIENT_ID = "1090177201451-bi2tt3gilbgk3gk868hsfuajk372hghj.apps.googleusercontent.com"
