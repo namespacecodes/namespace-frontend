50080bc74496dd62d722de5991522d723139934d
Sainath <sainath@analyticsquad4.com>
2023-10-05 21:05:50 +0530

    Loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import ImageComponent from './components/Authentication/CommonComponent'
 import LoginComponent from './components/Authentication/LoginComponent'
+import { Suspense } from 'react'
+import Loading from './components/Loading'
 
 export default function Home() {
   return (
+    <Suspense fallback={<Loading/>}>
+
     <div style={{width:"100vw",height:"100vh"}} className='flex flex-row justify-center items-center'>
     <ImageComponent/>
     <LoginComponent />
     </div>
+    </Suspense>
   )
 }
