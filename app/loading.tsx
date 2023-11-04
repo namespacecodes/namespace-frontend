8208a69150f551df0aa8bafa7ad93b4954b8c478
venkatarohan <venkatarohank@gmail.com>
2023-11-04 19:42:03 +0530

    test

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,4 @@
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
+  // You can add any UI inside Loading, including a Skeleton.lll
   return <p className="text-3xl">Hello Loading</p>
 }
