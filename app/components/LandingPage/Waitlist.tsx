d5c0c8f0f68af2a7c4322589e4510690fdf48318
Sainath <sainathreddypala@gmail.com>
2023-11-07 21:57:57 +0530

    waitlist api integration

--- app/components/LandingPage/Waitlist.tsx
+++ app/components/LandingPage/Waitlist.tsx
@@ -1,33 +1,52 @@
 "use client"
-import { Button, Form, Input } from 'antd'
+import { Button, Form, Input, message } from 'antd'
 import axios from 'axios';
-import { error } from 'console';
-import React from 'react'
-
+import React, {useState } from 'react'
+// https://name-space.onrender.com/
 const Waitlist = () => {
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const success = () => {
+      messageApi.open({
+        type: 'success',
+        content: 'Successfully joined waitlist',
+      });
+    };
+  
+    const error = (message:string) => {
+      messageApi.open({
+        type: 'error',
+        content: message,
+      });
+    };
+  
+    const [loading,setLoading]= useState<boolean>(false)
     const [form] =Form.useForm();
     const onFinish = (values: any) => {
-        console.log(values);
+        setLoading(true)
         axios.post('https://name-space.onrender.com/waitList',{
             name:values.name,
             email:values.email
         }).then((res)=>{
-            console.log("success");
-            
-        }).catch((error)=>{
-            console.log(error);
+            setLoading(false)
+            success()
+        }).catch((err)=>{
+            setLoading(false)
+            error(err?.response?.data?.status)
         })
     }
+
     return (
-        <Form form={form} onFinish={onFinish} style={{ display: "flex", flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }} className='space-y-3 pt-5'>
-            <Form.Item  name='name' className='w-5/6 md:w-1/6' rules={[{ required: true, message: 'Please input your name!' }]}>
+        <Form form={form} onFinish={onFinish} style={{ display: "flex", flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }} className='pt-5'>
+            {contextHolder}
+            <Form.Item  name='name' className='w-5/6 md:w-1/6' labelCol={{span:1}} rules={[{ required: true, message: 'Please input your name!' }]}>
                 <Input  placeholder='Name' />
             </Form.Item>
-            <Form.Item name='email' className='w-5/6 md:w-1/6' rules={[{ required: true, message: 'Please input your email!' }]}>
+            <Form.Item name='email' className='w-5/6 md:w-1/6' labelCol={{span:1}} rules={[{ required: true, message: 'Please input your email!' }]}>
                 <Input  placeholder='Email' />
             </Form.Item>
-            <Form.Item className='w-5/6 md:w-1/6'>
-                <Button className='bg-[#F6014E] w-full  text-[#FEFEFE]' htmlType='submit' type='default'>Join Waitlist</Button>
+            <Form.Item className='w-5/6 md:w-1/6' labelCol={{span:1}}>
+                <Button loading={loading} className='bg-[#F6014E] w-full  text-[#FEFEFE]' htmlType='submit' type='default'>Join Waitlist</Button>
             </Form.Item>
         </Form>
     )
