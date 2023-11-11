47983b4f89233a39ea7d5a5579fab943fe586c29
Sainath <sainathreddypala@gmail.com>
2023-11-11 21:57:38 +0530

    Added the images

--- app/components/LandingPage/Content.tsx
+++ app/components/LandingPage/Content.tsx
@@ -3,7 +3,7 @@ import { poppins_600, poppins_content_1, poppins_home, poppins_italic } from '@/
 import React from 'react'
 import Image from 'next/image';
 import { RightCircleOutlined } from '@ant-design/icons';
-import SphereComp from './SphereComp';
+// import SphereComp from './SphereComp';
 import Sphere1 from '../../../public/images/sphere_1.svg'
 import Sphere2 from '../../../public/images/sphere_2.svg'
 import { Carousel } from 'antd';
@@ -11,17 +11,17 @@ import { Carousel } from 'antd';
 const Content = () => {
     return (
         <div className='flex flex-1 h-[90%] py-7 px-3 w-screen flex-col md:flex-row' >
-            <div className={`${poppins_content_1.className} space-y-24 md:space-y-28 w-fit h-fit pt-[10%] pl-[10%] text-[#FFFFFF] md:w-1/2`}>
-                <div className='text-6xl space-y-7 hidden md:inline'>
+            <div className={`${poppins_content_1.className} space-y-24 md:space-y-[8vh] w-fit h-fit pt-[10%] pl-[10%] text-[#FFFFFF] md:w-1/2`}>
+                <div className='text-6xl space-y-[3vh] hidden md:inline'>
                     <div>Turn your name </div>
                     <div>
-                      into a   <span className='text-[#F6014E]'>{" "}brand</span>.
+                        into a   <span className='text-[#F6014E]'>{" "}brand</span>.
                     </div>
                 </div>
-                <div  className='text-6xl visible md:hidden '>
+                <div className='text-6xl visible md:hidden '>
                     <div>Turn your name into a <span className='text-[#F6014E]'>{" "}brand</span>. </div>
                 </div>
-                
+
                 <div className={`${poppins_home.className} break-normal text-[24px]`}>
                     <div><span className={poppins_italic.className}>Quickest</span> way to monetize your audience, All tools to build your personal brand in one space.</div>
                 </div>
@@ -38,11 +38,11 @@ const Content = () => {
                     </button>
                 </div>
             </div>
-            <div className='invisible md:w-1/2 md:visible'>
+            <div className='hidden md:w-1/2 md:inline'>
                 {/* <SphereComp /> */}
-                <Carousel autoplay dotPosition='left'>
-                    <Image src={Sphere1} alt="meetings" width="72" height="72" />
-                    <Image src={Sphere2} alt="meetings" width="72" height="72" />
+                <Carousel autoplay dotPosition='left' className='h-[90vh'>
+                    <Image src={Sphere1} fill={false} className='h-[80vh]' alt="meetings" />
+                    <Image src={Sphere2} fill={false} className='h-[80vh]' alt="meetings" />
                 </Carousel>
             </div>
         </div>
